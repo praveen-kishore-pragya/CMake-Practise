95640767906906d5fd89a88832c97f50be093593
Praveen Kishore Pragya <72307064+praveen-kishore-pragya@users.noreply.github.com>
2024-02-20 13:56:33 +0100

    Add files via upload
    Completed Step1 and added solutions for the TODO parts along with some Notes (as comments) for future reference.

---
+++ Step1/Step1_build/TutorialConfig.h
@@ -0,0 +1,15 @@
+// the configured options and settings for Tutorial
+// TODO 10: Define Tutorial_VERSION_MAJOR and Tutorial_VERSION_MINOR
+// When the project() command is called,
+//CMake defines Tutorial_VERSION_MAJOR and Tutorial_VERSION_MINOR behind the scenes.
+
+#define Tutorial_VERSION_MAJOR 5
+#define Tutorial_VERSION_MINOR 10
+
+//NOTE:
+/*The default value for CMAKE_PROJECT_VERSION_MAJOR and CMAKE_PROJECT_VERSION_MINOR is 0.
+If the version major and minor are not explicitly defined in the CMakeLists.txt,
+CMake will use these default values to replace the placeholders in the configured file.
+This ensures that there is always a valid value for the version components,
+even if they are not explicitly set in the CMakeLists.txt.*/
+
